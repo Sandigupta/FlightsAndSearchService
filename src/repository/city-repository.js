1783d8bb0e7c137c9fe4fd6123d4c1f8794e7e73
Sandigupta <sandeep.2022ug1100@iiitranchi.ac.in>
2024-12-30 07:09:16 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,9 +6,10 @@ async createCity({ name }) {
             const city = await City.create({ name });
             return city;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw { error };
         }
-    }
+}
 
 
 async deleteCity(cityId){
@@ -19,11 +20,36 @@ async deleteCity(cityId){
             }
         });
     } catch (error) {
+        console.log("Something went wrong in the repository layer");
         throw { error };
     }
     
 }
 
+    
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    } 
+    
+    async getCity(cityId) {
+        try {
+            const city = await City.findPk(cityId);
+            return City;
+        } catch {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
 }
 
 module.exports = CityRepository;
