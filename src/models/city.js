5d18638ac1aa09f3ed03443e49bf98cfe0c416b1
Sandigupta <sandeep.2022ug1100@iiitranchi.ac.in>
2025-01-03 18:11:46 +0530

    Added airport model

--- src/models/city.js
+++ src/models/city.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(module.Airport, {
+        foreignKey:'cityId'
+      });
     }
   }
   City.init({
